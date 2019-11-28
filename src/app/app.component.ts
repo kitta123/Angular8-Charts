9ec091e3e101e0c552d105b0978a396ce09cdf51
kitta123 <karthikvanu123@gmail.com>
2019-11-28 12:16:38 +0530

    Google- charts

---
+++ src/app/app.component.ts
@@ -0,0 +1,143 @@
+import { Component,OnInit } from '@angular/core';
+import {Chart} from 'chart.js';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Ng7ChartJs';
+  LineChart=[];
+  BarChart=[];
+  PieChart=[];
+
+  ngOnInit()
+  {
+     // Line chart:
+this.LineChart = new Chart('lineChart', {
+  type: 'line',
+data: {
+ labels: ["Jan", "Feb", "March", "April", "May", "June","July","Aug","Sep","Oct","Nov","Dec"],
+ datasets: [{
+     label: 'Number of Items Sold in Months',
+     data: [9,7 , 3, 5, 2, 10,15,16,19,3,1,9],
+     fill:false,
+     lineTension:0.2,
+     borderColor:"red",
+     borderWidth: 1
+ }]
+},
+options: {
+ title:{
+     text:"Line Chart",
+     display:true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero:true
+         }
+     }]
+ }
+}
+});
+
+// Bar chart:
+this.BarChart = new Chart('barChart', {
+  type: 'bar',
+data: {
+ labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+ datasets: [{
+     label: '# of Votes',
+     data: [9,7 , 3, 5, 2, 10],
+     backgroundColor: [
+         'rgba(255, 99, 132, 0.2)',
+         'rgba(54, 162, 235, 0.2)',
+         'rgba(255, 206, 86, 0.2)',
+         'rgba(75, 192, 192, 0.2)',
+         'rgba(153, 102, 255, 0.2)',
+         'rgba(255, 159, 64, 0.2)'
+     ],
+     borderColor: [
+         'rgba(255,99,132,1)',
+         'rgba(54, 162, 235, 1)',
+         'rgba(255, 206, 86, 1)',
+         'rgba(75, 192, 192, 1)',
+         'rgba(153, 102, 255, 1)',
+         'rgba(255, 159, 64, 1)'
+     ],
+     borderWidth: 1
+ }]
+},
+options: {
+ title:{
+     text:"Bar Chart",
+     display:true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero:true
+         }
+     }]
+ }
+}
+});
+
+// pie chart:
+this.PieChart = new Chart('pieChart', {
+  type: 'pie',
+data: {
+ labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+ datasets: [{
+     label: '# of Votes',
+     data: [9,7 , 3, 5, 2, 10],
+     backgroundColor: [
+         'rgba(255, 99, 132, 0.2)',
+         'rgba(54, 162, 235, 0.2)',
+         'rgba(255, 206, 86, 0.2)',
+         'rgba(75, 192, 192, 0.2)',
+         'rgba(153, 102, 255, 0.2)',
+         'rgba(255, 159, 64, 0.2)'
+     ],
+     borderColor: [
+         'rgba(255,99,132,1)',
+         'rgba(54, 162, 235, 1)',
+         'rgba(255, 206, 86, 1)',
+         'rgba(75, 192, 192, 1)',
+         'rgba(153, 102, 255, 1)',
+         'rgba(255, 159, 64, 1)'
+     ],
+     borderWidth: 1
+ }]
+},
+options: {
+ title:{
+     text:"Bar Chart",
+     display:true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero:true
+         }
+     }]
+ }
+}
+});
+
+this.kitta();
+
+  }
+  kitta()
+  {
+  google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(drawChart);
+  function drawChart() {
+   var data = google.visualization.arrayToDataTable([['Task', 'Hours per Day'], ['Work', 11], ['Eat', 2], ['Commute',  2], ['Watch TV', 2], ['Sleep', 7] ]);
+   var options = { title: 'My Daily Activities', pieHole: 0.4 };
+   var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+   chart.draw(data, options);
+  }
+}
+}
